336a49a4b86209c156043eaee3a7885e3cee94fc
itx-arpit <arpitiitb2026@gmail.com>
2025-04-06 14:49:32 +0530

    Update2 script.js

--- script.js
+++ script.js
@@ -39,8 +39,98 @@ const data = [
         category: 'fashion',
         link: "https://fktr.in/OYOi5Zj"
     },
-
-
+{
+        id: '5',
+        title: 'Frozen Girls Casual Cotton Lycra Blend Crop Top  (Multicolor, Pack of 1)',
+        image: 'pro5.png',
+        oldPrice: 999,
+        newPrice: 342,
+        discount: '65%',
+        category: 'fashion',
+        link: "https://fktr.in/Tn25VVW"
+    },
+    {
+        id: '6',
+        title: 'Women Regular Fit Self Design Casual Shirt',
+        image: 'pro6.png',
+        oldPrice: 1499,
+        newPrice: 370,
+        discount: '75%',
+        category: 'fashion',
+        link: "https://fktr.in/wx9U1Fr"
+    },
+    {
+        id: '7',
+        title: 'Women Printed Pure Cotton Straight Kurta  (Red)',
+        image: 'pro7.png',
+        oldPrice: 999,
+        newPrice: 328,
+        discount: '67%',
+        category: 'fashion',
+        link: "https://fktr.in/xapPHhf"
+    },
+    {
+        id: '8',
+        title: 'Forbuz Monster Truck Toy for Kids, Amazing Toys, 360 Degree Stunt Truck  (Red, Green, Violet, Orange',
+        image: 'pro8.png',
+        oldPrice: 399,
+        newPrice: 20,
+        discount: '94%',
+        category: 'Accessories',
+        link: "https://fktr.in/pub0QjN"
+    },
+    {
+        id: '9',
+        title: 'Leo Creation 144 TC Cotton Double Jaipuri Prints Flat Bedsheet  (Pack of 1, Blue, Green, Orange, Dark Blue, Black, Brown)',
+        image: 'pro9.png',
+        oldPrice: 2499,
+        newPrice: 299,
+        discount: '88%',
+        category: 'fashion',
+        link: "https://fktr.in/snMu5VT"
+    },
+      {
+        id: '10',
+        title: 'Embellished Bollywood Chiffon Saree  (Dark Green)',
+        image: 'pro10.png',
+        oldPrice: 3299,
+        newPrice: 489,
+        discount: '85%',
+        category: 'fashion',
+        link: "https://fktr.in/CLlHJyc"
+    },
+      {
+        id: '11',
+        title: 'Boys Festive & Party Kurta and Pyjama Set  (Green Pack of 1)',
+        image: 'pro11.png',
+        oldPrice: 1299,
+        newPrice: 633 ,
+        discount: '51%',
+        category: 'fashion',
+        link: "https://fktr.in/9mQoMTH"
+    },
+      {
+        id: '12',
+        title: 'Lite Sports Shoe Running Shoes For Men  (White , 6)',
+        image: 'pro12.png',
+        oldPrice: 2499,
+        newPrice: 492,
+        discount: '80%',
+        category: 'fashion',
+        link: "https://fktr.in/69iiG1w"
+    },
+      {
+        id: '13',
+        title: 'Men Regular Fit Printed Spread Collar Casual Shirt',
+        image: 'pro13.png',
+        oldPrice: 999,
+        newPrice: 222,
+        discount: '77%',
+        category: 'fashion',
+        link: "https://fktr.in/PWj5WPo"
+    },
+    
+    
     // Add more products...
   ];
      
